3da167b3c071e1f68ca4508ad35f1a88f920aace
JamesTorres99 <69390068+JamesTorres99@users.noreply.github.com>
2021-10-10 02:09:13 -0500

    add js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -110,11 +110,11 @@ var controlador = (function () {
 
             var s = "<div class='col-lg-3 col-md-6 mb-4 mb-lg-0'  >" +
                 "<div class='card rounded shadow-sm border-0' style='margin-bottom: 40px;'>" +
-                "<div class='card-body p-4'  style='width:100%;color:#290f68;'>" +
+                "<div class='card-body p-4'  style='width:100%;color:#290f68; background-color: rgba(0, 255, 213, 0.397);'>" +
                 "<img src='img/sub.png' alt='' class='img-fluid d-block mx-auto mb-3'>" +
                 "<h5 class='card-title'>" + name + "</h5>" +
                 "<p class='small text-muted font-italic'>" + description + "</p>" +
-                "<input onclick=\"window.location='./subasta.html?value=" + id + "';\"  type='button' value='ingresar' style='margin-left: 80px; margin-right: auto;'>" +
+                "<input onclick=\"window.location='./subasta.html?value=" + id + "';\"  type='button' value='ingresar' style='margin-left: auto; margin-right: auto;'>" +
                 "</div>" +
                 "</div>" +
                 "</div>";
